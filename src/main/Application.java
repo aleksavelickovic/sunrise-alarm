db438f20531e00b94e0a91764483e99d74b4b169
aleksavelickovic <velickovic.sr44.2023@uns.ac.rs>
2025-03-10 04:18:50 +0100

    Testing the GSON Library

--- src/main/Application.java
+++ src/main/Application.java
@@ -9,6 +9,10 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
 /**
  * 
  */
@@ -21,7 +25,9 @@ public class Application {
 	@SuppressWarnings("deprecation")
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		Gson gson = new Gson();
+		
+		
 		URL url = new URL("https://api.sunrise-sunset.org/json?lat=45.267136&lng=19.833549"); // lat & lng = Geografska
 																								// sirina i duzina
 
@@ -29,14 +35,19 @@ public class Application {
 		con.setRequestMethod("GET");
 		con.setRequestProperty("Content-Type", "application/json");
 
-		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
 		String inputLine;
 		String content = "";
-		while ((inputLine = in.readLine()) != null) {
+		while ((inputLine = reader.readLine()) != null) {
 			content += inputLine;
 		}
-		System.out.println(content);
-		in.close();
+		reader.close();
+		
+		JsonElement element = gson.fromJson (content, JsonElement.class);
+		JsonObject jsonObj = element.getAsJsonObject();
+		
+		System.out.println(jsonObj);
+		System.out.println(jsonObj.get("results"));
 	}
 
 }
