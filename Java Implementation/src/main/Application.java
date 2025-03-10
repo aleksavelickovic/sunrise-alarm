98ead8aa7138ba7bc672c04a1983349066771574
aleksavelickovic <velickovic.sr44.2023@uns.ac.rs>
2025-03-11 00:36:19 +0100

    Added parsing into local time.
    Now the program checks if the sunrise is now by checking the sunrise
    time against the system time.

--- Java Implementation/src/main/Application.java
+++ Java Implementation/src/main/Application.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
